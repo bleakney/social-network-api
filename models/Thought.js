d22659ed1e50c4986722e4536e176e7b6d20c0ea
Bleakney Bissett <bleakneybissett@gmail.com>
2021-09-08 14:56:36 -0500

    create Thought and Reaction models

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,67 @@
 const { Schema, model } = require('mongoose');
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
+// update validators on userjs
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dayjs(createdAtVal).format('MMMM D, YYYY h:mm A')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dayjs(createdAtVal).format('MMMM D, YYYY h:mm A')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model("thought", ThoughtSchema);
+
+module.exports = Thought;
