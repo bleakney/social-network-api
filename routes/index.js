a1a5ba36816186f92cd7780995a56182690aa6e3
Bleakney Bissett <bleakneybissett@gmail.com>
2021-09-08 15:21:48 -0500

    create index for routing

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+
+router.use('/api/thoughts', thoughtRoutes);
+router.use('/api/users', userRoutes);
+
+
+
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error</h1');
+});
+
+module.exports = router;
